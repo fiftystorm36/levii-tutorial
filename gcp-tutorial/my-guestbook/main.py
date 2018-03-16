16773b628e6a24f5c16ad539807eff83a63b7531
fiftystorm <kenyayanke223@gmail.com>
2018-03-16 09:44:12 +0900

    残TODO: greeting の数を表示する

--- gcp-tutorial/my-guestbook/main.py
+++ gcp-tutorial/my-guestbook/main.py
@@ -24,6 +24,7 @@ For more information, see README.md
 import cgi
 import textwrap
 import urllib
+import time
 
 from google.appengine.ext import ndb
 
@@ -50,10 +51,10 @@ class Guestbook(ndb.Model):
 
 
 class GuestbookPage(webapp2.RequestHandler):
-    def get(self):
-        self.response.out.write('<html><body>')
+    def get(self, guestbook_id):
+        guestbook = Guestbook.get_by_id(long(guestbook_id))
         guestbook_name = self.request.get('guestbook_name')
-        ancestor_key = ndb.Key("Book", guestbook_name or "*notitle*")
+        ancestor_key = guestbook.key
         greetings = Greeting.query_book(ancestor_key).fetch(20)
 
         greeting_blockquotes = []
@@ -61,12 +62,12 @@ class GuestbookPage(webapp2.RequestHandler):
             greeting_blockquotes.append(
                 '<blockquote>%s</blockquote>' % cgi.escape(greeting.content))
 
-        self.response.out.write(textwrap.dedent("""\
+        self.response.out.write(textwrap.dedent("""
             <html>
               <body>
                 <h1>{guestbook_name}</h1>
                 {blockquotes}
-                <form action="/sign?{sign}" method="post">
+                <form action="/sign?%s" method="post">
                   <div>
                     <textarea name="content" rows="3" cols="60">
                     </textarea>
@@ -76,28 +77,30 @@ class GuestbookPage(webapp2.RequestHandler):
                   </div>
                 </form>
                 <hr>
-                <input type="button" value="back to list" onClick="location.href='/list'">
+                <input type="button" value="back to list" onClick="location.href='/'">
               </body>
-            </html>""").format(
+            </html>""" % urllib.urlencode({'guestbook_id': guestbook_id})).format(
+            guestbook_name=cgi.escape(guestbook_name),
             blockquotes='\n'.join(greeting_blockquotes),
-            sign=urllib.urlencode({'guestbook_name': guestbook_name}),
-            guestbook_name=cgi.escape(guestbook_name)))
+            sign=urllib.urlencode({'guestbook_name': guestbook_name})
+            ))
+
 
 class ListPage(webapp2.RequestHandler):
     def get(self):
-        self.response.out.write('<html><body>')
         guestbooks = Guestbook.query_book()
 
-        guestbook_blockquotes = []
+        guestbook_links = []
         for guestbook in guestbooks:
-            guestbook_blockquotes.append(
-                '<blockquote>%s</blockquote>' % cgi.escape(guestbook.name))
+            guestbook_links.append(
+                '- <a href="/books/%s">%s</a>' % (guestbook.key.id(), cgi.escape(guestbook.name))
+            )
 
-        self.response.out.write(textwrap.dedent("""\
+        self.response.out.write(textwrap.dedent("""
             <html>
               <body>
               <h1>Guestbook List</h1>
-                {blockquotes}
+                {links}
                 <form action="/create" method="post">
                   <div>
                     <textarea name="guestbook_name" rows="1" cols="60">
@@ -106,38 +109,44 @@ class ListPage(webapp2.RequestHandler):
                   <div>
                     <input type="submit" value="Create New Guestbook">
                   </div>
-                </form>
               </body>
             </html>""").format(
-            blockquotes='\n'.join(guestbook_blockquotes)))
-            #create=urllib.urlencode({'guestbook_name': guestbook_name}),
-            #guestbook_name=cgi.escape(guestbook_name)))
+            links='<br>'.join(guestbook_links)))
+        # create=urllib.urlencode({'guestbook_name': guestbook_name}),
+        # guestbook_name=cgi.escape(guestbook_name)))
 
 
 class SubmitForm(webapp2.RequestHandler):
     def post(self):
-        # We set the parent key on each 'Greeting' to ensure each guestbook's
-        # greetings are in the same entity group.
-        guestbook_name = self.request.get('guestbook_name')
-        greeting = Greeting(parent=ndb.Key("Book",
-                                           guestbook_name or "*notitle*"),
+        guestbook_id = self.request.get('guestbook_id')
+        guestbook = Guestbook.get_by_id(long(guestbook_id))
+        greeting = Greeting(parent=guestbook.key,
                             content=self.request.get('content'))
         greeting.put()
-        self.redirect('/?' + urllib.urlencode(
-            {'guestbook_name': guestbook_name}))
+        self.redirect('/books/' + str(guestbook_id))
+
 
 class CreateForm(webapp2.RequestHandler):
     def post(self):
-        # We set the parent key on each 'Greeting' to ensure each guestbook's
-        # greetings are in the same entity group.
         guestbook_name = self.request.get('guestbook_name')
         guestbook = Guestbook(name=guestbook_name)
         guestbook.put()
+        time.sleep(0.5)  # wait for put() are finished
         self.redirect('/')
 
+
 app = webapp2.WSGIApplication([
-    ('/book', GuestbookPage),
-    ('/', ListPage),
-    ('/sign', SubmitForm),
-    ('/create', CreateForm)
+    (r'/books/(\d+)', GuestbookPage),
+    (r'/', ListPage),
+    (r'/sign', SubmitForm),
+    (r'/create', CreateForm)
 ])
+
+'''
+app = webapp2.WSGIApplication([
+    webapp2.Route(r'/books/<guestbook_id:\d+>', handler=GuestbookPage, name='books'),
+    webapp2.Route(r'/', handler=ListPage, name='book-list'),
+    webapp2.Route(r'/sign', handler=SubmitForm, name='sign'),
+    webapp2.Route(r'/create', handler=CreateForm, name='create')
+])
+'''
