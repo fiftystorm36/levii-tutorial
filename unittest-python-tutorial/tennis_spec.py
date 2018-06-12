4b9e8d10fd9938a04427313cd2a3bace73bdd8c1
fiftystorm <kenyayanke223@gmail.com>
2018-06-12 09:13:20 +0900

    実際の処理を追加

--- unittest-python-tutorial/tennis_spec.py
+++ unittest-python-tutorial/tennis_spec.py
@@ -1,10 +1,12 @@
 from mamba import description, context, it
 from expects import expect, equal
 
+import tennis
+
 with description('Tennis') as self:
     with it('starts with 0 - 0 score'):
         rafa_nadal = "Rafa Nadal"
         roger_federer = "Roger Federer"
-        game = Game(rafa_nadal, roger_federer)
+        game = tennis.Game(rafa_nadal, roger_federer)
 
         expect(game.score()).to(equal((0, 0)))
