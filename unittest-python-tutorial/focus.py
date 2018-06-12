371df7c7f323c3467f1b33d03e342e66cbca7032
fiftystorm <kenyayanke223@gmail.com>
2018-06-12 10:43:15 +0900

    特定のテストだけ実行する

---
+++ unittest-python-tutorial/focus.py
@@ -0,0 +1,22 @@
+from mamba import description, context, it, fit
+from expects import expect, equal
+
+#import tennis
+
+# executed by 'mamba filter.py -t integration'
+with description('Tennis1', 'integration') as self:
+    with fit('starts with 0 - 0 score'):
+        rafa_nadal = "Rafa Nadal"
+        roger_federer = "Roger Federer"
+        game = tennis.Game(rafa_nadal, roger_federer)
+
+        expect(game.score()).to(equal((0, 0)))
+
+# executed by 'mamba filter.py -t unit'
+with description('Tennis2', 'unit') as self:
+    with it('starts with 0 - 0 score'):
+        rafa_nadal = "Rafa Nadal"
+        roger_federer = "Roger Federer"
+        game = tennis.Game(rafa_nadal, roger_federer)
+
+        expect(game.score()).to(equal((0, 0)))
